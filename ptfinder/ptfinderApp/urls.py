d124f79a7bcd43519905902376a877db80352d08
KierWats <46569382+KierWats@users.noreply.github.com>
2020-03-20 18:21:35 +0000

    Urls (#3)
    * Update urls.py
    
    * Update urls.py
    
    * Update urls.py
    
    * removing template
    
    Co-authored-by: lewis-tse <43795292+lewis-tse@users.noreply.github.com>

--- ptfinder/ptfinderApp/urls.py
+++ ptfinder/ptfinderApp/urls.py
@@ -4,5 +4,20 @@ from ptfinderApp import views
 app_name = 'ptfinder'
 
 urlpatterns = [
-    path('', views.index, name="index"),
+    path('', views.index, name='index'),
+    path('user/', views.user, name='user'),
+    path('user/login/', views.login , name='login'),
+    path('user/register/', views.register , name='register'),
+    path('user/<str:username>/', views.user_profile , name='user_profile'),
+    path('user/<str:username>/edit_account/', views.'edit_account , name='edit_account'),
+    path('trainer/', views.trainer , name='trainer'),
+    path('trainer/<str:t_username>/', views.trainer_profile , name='trainer_profile'), 
+    path('book/', views.book , name='book'),
+    path('book/create_booking/', views.create_booking, name='create_booking'),
+    path('book/<str:username>/', views.view_bookings , name='view_bookings'),
+    path('book/<str:t_username>/', views.view_bookings , name='view_bookings'),
+    path('contact_us/', views.contact_us , name='contact_us'),
+    path('gym/', views.gym , name='gym'), 
+    path('gym/<int:g_id>/', views.gym_profile , name='gym_profile'), 
+    path('logout/', views.logout , name='logout'),
 ]
