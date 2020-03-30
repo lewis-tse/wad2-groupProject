c253cb7bc3bfb45b3ebec59460f6f8cb628eaba1
KierWats <46569382+KierWats@users.noreply.github.com>
2020-03-30 15:45:59 +0100

    Update urls.py

--- ptfinder/ptfinderApp/urls.py
+++ ptfinder/ptfinderApp/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('user/login/', views.login , name='login'),
     path('user/register/', views.register , name='register'),
     path('user/<str:username>/', views.user_profile , name='user_profile'),
-    path('user/<str:username>/edit_account/', views.edit_account , name='edit_account'),
+    path('user/<str:username>/edit_profile/', views.edit_profile , name='edit_profile'),
     path('trainer/', views.trainer , name='trainer'),
     path('trainer/<str:t_username>/', views.trainer_profile , name='trainer_profile'), 
     path('book/', views.book , name='book'),
@@ -20,5 +20,4 @@ urlpatterns = [
     path('contact_us/', views.contact_us , name='contact_us'),
     path('gym/', views.gym , name='gym'), 
     path('gym/<int:g_id>/', views.gym_profile , name='gym_profile'), 
-    path('logout/', views.logout , name='logout'),
 ]
