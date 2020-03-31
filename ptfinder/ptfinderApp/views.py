36dd9e15473cbb419aed31a861c3e215886fcd9f
KierWats <46569382+KierWats@users.noreply.github.com>
2020-03-31 13:03:57 +0100

    View templates (#6)
    * added view templates
    
    * Update views.py
    
    * Update views.py
    
    * Update views.py
    
    * Update views.py
    
    Co-authored-by: lewis-tse <43795292+lewis-tse@users.noreply.github.com>

--- ptfinder/ptfinderApp/views.py
+++ ptfinder/ptfinderApp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from ptfinderApp.forms import UserForm, UserProfileForm, TrainerProfileForm
 
 def index(request):
     context_dict = {}
@@ -24,14 +25,60 @@ def login(request):
     return render(request, 'ptfinderApp/login.html', context=context_dict)
     
 def register(request):
-    context_dict = {}
-
-    return render(request, 'ptfinderApp/register.html', context=context_dict)
+    registered = False
+    if request.method == 'POST':
+        user_form = UserForm(request.POST)
+        profile_form = UserProfileForm(request.POST)
+        
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+            profile = profile_form.save(commit=False)
+            profile.account = user
+            
+            if 'picture' in request.FILES:
+                profile.picture = request.FILES['picture']
+                
+            profile.save()
+            registered = True
+        else:
+            print(user_form.errors, profile_form.errors)
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileForm()
     
-def trainer_register(request):
-    context_dict = {}
+    context_dict = {'user_form':user_form,'profile_form':profile_form,'registered':registered}
+    
+    return render(request, 'ptfinderApp/register.html', context=context_dict)
 
-    return render(request, 'ptfinderApp/trainer_register.html', context=context_dict)
+def trainer_register(request):
+    registered = False
+    if request.method == 'POST':
+        user_form = UserForm(request.POST)
+        profile_form = TrainerProfileForm(request.POST)
+        
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+            profile = profile_form.save(commit=False)
+            profile.t_account = user
+            
+            if 'picture' in request.FILES:
+                profile.picture = request.FILES['picture']
+                
+            profile.save()
+            registered = True
+        else:
+            print(user_form.errors, profile_form.errors)
+    else:
+        user_form = UserForm()
+        profile_form = TrainerProfileForm()
+    
+    context_dict = {'user_form':user_form,'profile_form':profile_form,'registered':registered}
+
+    return render(request, 'ptfinderApp/trainer-register.html', context=context_dict)
 
 def edit_profile(request):
     context_dict = {}
