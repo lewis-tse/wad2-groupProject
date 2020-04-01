fd17fa3cd757104ad05d61f2c0a59850825c50f9
KierWats <46569382+KierWats@users.noreply.github.com>
2020-04-01 22:02:09 +0100

    Update urls.py
    Added , namespace="ptfinderApp" to path('ptfinder/'

--- ptfinder/ptfinder/urls.py
+++ ptfinder/ptfinder/urls.py
@@ -22,6 +22,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('ptfinder/', include('ptfinderApp.urls')),
+    path('ptfinder/', include('ptfinderApp.urls', namespace="ptfinderApp")),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
