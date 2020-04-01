bb0fed31d183250bdc11e4c02545224ae3de41b5
KierWats <46569382+KierWats@users.noreply.github.com>
2020-04-01 22:00:03 +0100

    Update urls.py

--- ptfinder/ptfinderApp/urls.py
+++ ptfinder/ptfinderApp/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from ptfinderApp import views
+from django.urls import re_path
 
 app_name = 'ptfinder'
 
@@ -7,18 +8,17 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('user/', views.user, name='user'),
     path('user/login/', views.login , name='login'),
-    path('user/register/trainer', views.trainer_register , name='trainer_register'),
     path('user/register/', views.register , name='register'),
     path('user/register/trainer', views.trainer_register , name='trainer_register'),
-    path('user/<str:username>/', views.user_profile , name='user_profile'),
-    path('user/<str:username>/edit_profile/', views.edit_profile , name='edit_profile'),
+    re_path('user/(P?<username>.*)/$', views.user_profile , name='user_profile'),
+    re_path('user/(P?<username>.*)/edit_profile/$', views.edit_profile , name='edit_profile'),
     path('trainer/', views.trainer , name='trainer'),
-    path('trainer/<str:t_username>/', views.trainer_profile , name='trainer_profile'), 
+    re_path('trainer/(P?<account>.*)/$', views.trainer_profile , name='trainer_profile'), 
     path('book/', views.book , name='book'),
-    path('book/create_booking/', views.create_booking, name='create_booking'),
-    path('book/<str:username>/', views.view_bookings , name='view_bookings'),
-    path('book/<str:t_username>/', views.view_bookings , name='view_bookings'),
+    re_path(r'^book/(?P<account>.*)/create_booking/$', views.create_booking, name='create_booking'),
+    re_path(r'^book/(P?<username>.*)/$', views.view_bookings , name='view_bookings'),
+    re_path('book/(P?<account>.*)/$', views.view_bookings , name='view_bookings'),
     path('contact_us/', views.contact_us , name='contact_us'),
     path('gym/', views.gym , name='gym'), 
-    path('gym/<int:g_id>/', views.gym_profile , name='gym_profile'), 
+    re_path('gym/(P?<name>.*)/$', views.gym_profile , name='gym_profile'), 
 ]
